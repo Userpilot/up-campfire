1d2c09ad12a62f4f330f6e751ef3167000c47d74
Walaa <walaa@yamsafer.me>
2020-08-27 17:26:54 +0300

    token to env config

--- packages/isomorphic-next/env-config.js
+++ packages/isomorphic-next/env-config.js
@@ -2,4 +2,7 @@ const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
   BACKEND_URL: prod ? 'https://api.example.com' : 'https://localhost:8080',
+  env: {
+    NEXT_PUBLIC_TOKEN: 'value',
+  },
 };
