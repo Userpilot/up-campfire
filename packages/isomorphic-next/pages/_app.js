36582c5a25ac2cf2275c923d3d3c777b35ad2b1d
Walaa <walaa@yamsafer.me>
2020-11-05 17:50:04 +0200

    initialize userpilot before

--- packages/isomorphic-next/pages/_app.js
+++ packages/isomorphic-next/pages/_app.js
@@ -30,10 +30,13 @@ const CustomApp = ({ Component, pageProps, store }) => {
   useEffect(() => {
     if (!window.userpilot) {
       const { Userpilot } = require('userpilot');
-      if ((process.env.NEXT_PUBLIC_SDK_PRODUCTION || 'true').toLowerCase() === 'false') {
+      Userpilot.initialize(process.env.NEXT_PUBLIC_TOKEN);
+      if (
+        (process.env.NEXT_PUBLIC_SDK_PRODUCTION || 'true').toLowerCase() ===
+        'false'
+      ) {
         window.userpilotSettings.version = 'staging';
       }
-      Userpilot.initialize(process.env.NEXT_PUBLIC_TOKEN);
       console.log(window.drift);
     }
     if (installPendo && window !== undefined) {
