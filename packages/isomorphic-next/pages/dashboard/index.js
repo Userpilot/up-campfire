8d1cfa14010ad52df9a9e976016b3d9234ee3c74
Walaa <walaa@yamsafer.me>
2020-12-02 11:06:23 +0200

    remove the referral check 3 login

--- packages/isomorphic-next/pages/dashboard/index.js
+++ packages/isomorphic-next/pages/dashboard/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import loadable from '@loadable/component';
 import { withAuthSync } from '../../authentication/auth.utils';
 import DashboardLayout from '../../containers/DashboardLayout/DashboardLayout';
@@ -9,23 +8,18 @@ import { getCurrentUser } from '@iso/lib/firebase/firebase.authentication.util';
 const Widgets = loadable(() => import('@iso/containers/Widgets/Widgets'));
 
 export default withAuthSync(() => {
-  const router = useRouter();
-
-  if (router.query && router.query.referal === 'login') {
-    getCurrentUser().then((user) => {
-      if (window && window.userpilot) {
-        window.userpilot.identify(user.uid, {
-          name: user.email,
-          email: user.email,
-          company: {
-            id: 111111111,
-          },
-          plan: 'free',
-        });
-      }
-    });
-  }
-
+  getCurrentUser().then((user) => {
+    if (window && window.userpilot) {
+      window.userpilot.identify(user.uid, {
+        name: user.email,
+        email: user.email,
+        company: {
+          id: 111111111,
+        },
+        plan: 'free',
+      });
+    }
+  });
   return (
     <>
       <Head>
