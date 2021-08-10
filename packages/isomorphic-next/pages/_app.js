3a1677621fcde09b91e35613aedfb98fc0768ff1
Hammam <h.samara.74@gmail.com>
2021-08-10 14:07:01 +0300

    fullsession for one token

--- packages/isomorphic-next/pages/_app.js
+++ packages/isomorphic-next/pages/_app.js
@@ -83,6 +83,24 @@ const CustomApp = ({ Component, pageProps, store }) => {
         },
       });
     }
+
+    if (process.env.NEXT_PUBLIC_TOKEN == '100jo81c7' && window !== undefined) {
+        <script type="text/javascript">
+        (function()
+        {(function (m, n, t, l, x, p, o){
+            window["_fs_host"] = l;
+            window["_site_id"] = p;
+            window["_fs_id"] = x;
+            o = n.createElement(t);
+            o.type = "text/javascript";
+            o.async = true;
+            o.src = "https://rt.fullsession.io/main.js";
+            y = n.getElementsByTagName(t)[0];
+            y.parentNode.insertBefore(o, y);
+            })(window, document, "script", "fullsession.io", 'kroohjsaarp', 'kroohw0nxq')};
+            )();
+        </script>
+    }
   }, []);
   return (
     <Provider store={store}>
