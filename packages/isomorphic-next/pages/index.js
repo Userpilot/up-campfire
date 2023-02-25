0eeae4332c373fb5ee2b742d34a9b3286375460d
ThabetIbrahim <thabet.ig@gmail.com>
2023-02-25 15:20:46 +0200

    Update index.js

--- packages/isomorphic-next/pages/index.js
+++ packages/isomorphic-next/pages/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import Signin from './signin';
 import Dashboard from './dashboard';
+import { useRouter } from 'next/router';
 
-export default () => <Dashboard />;
+export default () => {
+  const router = useRouter();
+  switch (router.asPath) {
+    case '/signin':
+      return <Signin />;
+    default:
+      return <Dashboard />;
+  }
+};
