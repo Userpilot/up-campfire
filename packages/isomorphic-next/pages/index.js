6a50fe9b85bcd15c8788d4495e3bd6828ee716dc
ThabetIbrahim <thabet.ig@gmail.com>
2023-02-25 15:14:03 +0200

    redirect to dashboard instead of signin

--- packages/isomorphic-next/pages/index.js
+++ packages/isomorphic-next/pages/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import Signin from './signin';
 import Dashboard from './dashboard';
-import Router from 'next/router';
 
-switch (Router.pathname) {
-  case '/signin':
-    <Signin />;
-  default:
-    <Dashboard />;
-}
+export default () => <Dashboard />;
